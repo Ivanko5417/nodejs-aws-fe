56dbfd555bacfacf857df5a95fc4f46e082115c4
Ivanko5417 <v.drobysh2011@gmail.com>
2020-11-27 23:56:14 +0300

    feat: add auth headr and alrt in case of 401 or 403 errors

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://7v0tst1fs1.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://1ml9619jd3.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://9f13z1e5lf.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
